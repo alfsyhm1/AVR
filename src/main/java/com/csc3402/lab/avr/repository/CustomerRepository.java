6249f9a25e1779c8d78ac09260bc22866620bcdc
alfsyhm1 <215723@student.upm.edu.my>
2024-06-23 01:03:36 +0800

    html and css

--- src/main/java/com/csc3402/lab/avr/repository/CustomerRepository.java
+++ src/main/java/com/csc3402/lab/avr/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.csc3402.lab.avr.repository;
 
 import com.csc3402.lab.avr.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    // Additional custom query methods can be defined here if needed
+
 }
